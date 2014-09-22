1cc8ab8a2f1fdb9735008cefa549ae06581fefe3
Timothy Chen <tnachen@gmail.com>
2014-09-21 21:05:34 -0700

    Add back nullable type

--- exec/java-exec/src/main/java/org/apache/drill/exec/expr/fn/impl/waggr/WindowRankingFunctions.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/expr/fn/impl/waggr/WindowRankingFunctions.java
@@ -19,16 +19,11 @@
 
 package org.apache.drill.exec.expr.fn.impl.waggr;
 
-import org.apache.drill.exec.expr.DrillFunc;
 import org.apache.drill.exec.expr.DrillWindowPointAggFunc;
 import org.apache.drill.exec.expr.annotations.FunctionTemplate;
 import org.apache.drill.exec.expr.annotations.Output;
-import org.apache.drill.exec.expr.annotations.Param;
 import org.apache.drill.exec.expr.annotations.Workspace;
 import org.apache.drill.exec.expr.holders.BigIntHolder;
-import org.apache.drill.exec.expr.holders.Float4Holder;
-import org.apache.drill.exec.expr.holders.Float8Holder;
-import org.apache.drill.exec.expr.holders.IntHolder;
 import org.apache.drill.exec.record.RecordBatch;
 
 public class WindowRankingFunctions {
